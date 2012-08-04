006fa0795a91c3dbb9a04fcc60622428a205d5ad
Ran Meyerstein <ranmeyerstein@gmail.com>
2012-08-04 13:30:27 +0300

    TCP Listener

--- ProRBT/ProRBT.cpp
+++ ProRBT/ProRBT.cpp
@@ -4,47 +4,132 @@
 #include "stdafx.h"
 #include <iostream>
 #include <fstream>
+#include "Socket.h"
+#include "Iphlpapi.h"
 #include "windows.h"
 
 using namespace std;
 
-int _tmain(int argc, _TCHAR* argv[])
+typedef struct
 {
-	_TCHAR lBarcode[14], Qty[4], SessionId[17], LineNum[5], TotalLines[5], Directive[2];
-	wofstream ParFile;
+	_TCHAR Barcode[14], Qty[4], SessionId[17], LineNum[5], TotalLines[5], Directive[2];
+}PRORBTPARAMS, *pPRORBTPARAMS;
 
-	ParFile.open ("ProRBT.log",ios::app);
+BOOL GetDesktopIp(char *ipAddr, int len);
 
-	if (argc < 6) { // Check the value of argc. If not enough parameters have been passed, inform user and exit.
-		std::cout << "Usage is [Barcode] [Qty] [ID] [Line no.] [Total no.] [Directive]\n"; // Inform the user of how to use the program
-		std::cin.get();
-		exit(0);
-	} else {
-		memcpy((void*)lBarcode, argv[1], 14 * sizeof (_TCHAR));
-		memcpy((void*)Qty, argv[2], 4 * sizeof (_TCHAR));
-		memcpy((void*)SessionId, argv[3], 17 * sizeof (_TCHAR));
-		memcpy((void*)LineNum, argv[4], 5 * sizeof (_TCHAR));
-		memcpy((void*)TotalLines, argv[5], 5 * sizeof (_TCHAR));
-		memcpy((void*)Directive, argv[6], 2 * sizeof (_TCHAR));
-		std::wcout << L"Bardcode [" << (LPCTSTR)argv[1] << "]" << endl;
-		std::wcout << "Quantity [" << Qty << "]" << endl;
-		std::wcout << "Session ID [" << SessionId << "]" << endl;
-		std::wcout << "Line number[" << LineNum << "]" << endl;
-		std::wcout << "Total number of lines [" << TotalLines << "]" << endl;
-		if ((Directive[0] != '1') && (Directive[0] != '2'))
-		std::wcout << L"Bad Directive[" << argv[6] << "] Expected: 1 - Query, 2 - Dispense" << endl;
+Socket _socket;
+char desktopIp[16] = {0};
+int port = 50004;
+
+DWORD WINAPI SocketThread(HANDLE hExitEvent)
+{
+	while (WaitForSingleObject(hExitEvent, 100) == WAIT_TIMEOUT)
+	{
+		if (_socket.IsConnected())
+		{
+			char *buffer = (char *)_socket.Receive(6);
+			if (!buffer)
+				continue;
+
+			if (buffer[0] == '`')
+			{
+
+			}
+
+			free(buffer);
+		}
 		else
-		std::cout << "Directive [" << (Directive[0] == '1' ? "Query": "Dispense") << "]" << endl;
-
-		ParFile << lBarcode << L" ; " << Qty <<  L" ; " << SessionId <<  L" ; " << LineNum << 
-			L" ; " << TotalLines <<  L" ; " << Directive << endl;
-		ParFile.close();
-		exit(0);
+		{
+			_socket.Disconnect();
+			_socket.Connect(desktopIp, port);
+		}
 	}
-	
-	std::cin.get();
+
 	return 0;
 }
 
 
+int _tmain(int argc, _TCHAR* argv[])
+{
+	PRORBTPARAMS ProRbtParams;
+	wofstream ParFile;
+	static HANDLE hSocketThread; 
 
+	if (!GetDesktopIp(desktopIp, sizeof(desktopIp)))
+	{
+		std::wcout << "Failed to obtain IP. Exiting!" << endl;
+		std::cin.get();
+		exit(0);
+	}
+
+	hSocketThread = CreateThread(NULL, 0, SocketThread, NULL, 0, NULL);
+
+	ParFile.open ("ProRBT.log",ios::app);
+
+	if (argc < 6) { // Check the value of argc. If not enough parameters have been passed, inform user and exit.
+		std::cout << "Usage is [Barcode 13] [Qty 3] [ID 16] [Line no. 4] [Total no. 4] [Directive 1]\n"; // Inform the user of how to use the program
+		std::cin.get();
+		exit(0);
+	} else {
+		memcpy((void*)ProRbtParams.Barcode, argv[1], 14 * sizeof (_TCHAR));
+		memcpy((void*)ProRbtParams.Qty, argv[2], 4 * sizeof (_TCHAR));
+		memcpy((void*)ProRbtParams.SessionId, argv[3], 17 * sizeof (_TCHAR));
+		memcpy((void*)ProRbtParams.LineNum, argv[4], 5 * sizeof (_TCHAR));
+		memcpy((void*)ProRbtParams.TotalLines, argv[5], 5 * sizeof (_TCHAR));
+		memcpy((void*)ProRbtParams.Directive, argv[6], 2 * sizeof (_TCHAR));
+		std::wcout << L"Bardcode [" << (LPCTSTR)argv[1] << "]" << endl;
+		std::wcout << "Quantity [" << ProRbtParams.Qty << "]" << endl;
+		std::wcout << "Session ID [" << ProRbtParams.SessionId << "]" << endl;
+		std::wcout << "Line number[" << ProRbtParams.LineNum << "]" << endl;
+		std::wcout << "Total number of lines [" << ProRbtParams.TotalLines << "]" << endl;
+		if ((ProRbtParams.Directive[0] != '1') && (ProRbtParams.Directive[0] != '2'))
+		std::wcout << L"Bad Directive[" << argv[6] << "] Expected: 1 - Query, 2 - Dispense" << endl;
+		else
+		std::cout << "Directive [" << (ProRbtParams.Directive[0] == '1' ? "Query": "Dispense") << "]" << endl;
+
+		ParFile << ProRbtParams.Barcode << L" ; " << ProRbtParams.Qty <<  L" ; " << ProRbtParams.SessionId <<
+			L" ; " << ProRbtParams.LineNum << L" ; " << ProRbtParams.TotalLines <<  L" ; " << ProRbtParams.Directive << endl;
+	}
+	
+	CloseHandle(hSocketThread);
+	ParFile.close();
+	return 0;
+}
+
+BOOL GetDesktopIp(char *ipAddr, int len)
+{
+	BOOL ret = FALSE;
+
+	IP_ADAPTER_INFO *pAdapterInfo = (IP_ADAPTER_INFO *)new BYTE[sizeof(IP_ADAPTER_INFO)];
+	ULONG OutBufLen = 0;
+	DWORD res = GetAdaptersInfo(pAdapterInfo, &OutBufLen);
+	if (res == ERROR_BUFFER_OVERFLOW)
+	{
+		delete (pAdapterInfo);
+		pAdapterInfo = (IP_ADAPTER_INFO *)new BYTE[OutBufLen];
+		res = GetAdaptersInfo(pAdapterInfo, &OutBufLen);
+	}
+
+	if (res == NO_ERROR)
+	{
+		IP_ADAPTER_INFO *info = pAdapterInfo;
+		do
+		{
+			//RETAILMSG(1, (L"AdapterName = %s\r\n", info->AdapterName));
+			//if (strstr(info->AdapterName, "USB CABLE"))
+			if (strcmp(info->GatewayList.IpAddress.String,"0.0.0.0") != 0)
+			{
+				strncpy_s(ipAddr, len, info->IpAddressList.IpAddress.String, len);
+				ret = TRUE;
+				//break;
+			}
+
+			info = info->Next;
+		}
+		while (info);
+
+	}
+
+	delete (pAdapterInfo);
+	return ret;
+}
